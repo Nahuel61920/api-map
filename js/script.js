2285c943afa6452c0b6e4c6ab0754fd9b29b85c3
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-01-22 05:07:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ function iniciarMap(){
     const map = new google.maps.Map(document.getElementById("map"),
     {zoom: 15,
     center: coord});
+
+    const marker = new google.maps.Marker({
+        position: coord,
+        map: map
+    });
 }
